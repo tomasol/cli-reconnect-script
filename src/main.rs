8d59b1d954ebd6b5349084d880d9474de897e2b4
Tomas Olvecky <tomas.olvecky@gmail.com>
2021-06-10 16:23:57 +0200

    Simplify wait_for_prompt - move code to get_last_log

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::str;
 use std::thread;
 use std::time::{Duration, SystemTime};
 
+
 fn mount() {
     let output = Command::new("curl")
         .args(&[
@@ -24,7 +25,7 @@ fn mount() {
                   "cli-topology:password" : "ciscocisco",
                   "node-extension:reconcile": false,
                   "cli-topology:journal-size": 150,
-                  "cli-topology:dry-run-journal-size": 150,
+                  "cli-topology:dry-run-journal-size": 0,
                   "cli-topology:keepalive-timeout" : 180
                 }
             }"#
@@ -39,9 +40,13 @@ fn mount() {
     info!("mount ok");
 }
 
-enum WaitForPromptResult {
+
+#[derive(Debug)]
+enum LogResult {
     PromptResolved,
-    Timeout,
+    ExposingMountpointUnder,
+    DeviceSuccessfullyMounted,
+    Unknown,
 }
 
 // Monitor log file. Wait until the prompt is resolved. Normally this ends in ~10s.
@@ -50,40 +55,7 @@ enum WaitForPromptResult {
 // "Exposing mountpoint under"
 // hopefully this state will not execute:
 // "Device successfully mounted" // too late
-fn wait_for_prompt() -> WaitForPromptResult {
-    let now = SystemTime::now();
-    let max_duration = Duration::from_secs(15);
-    while now.elapsed().unwrap() < max_duration {
-        let output = Command::new("tail")
-        .args(&[
-            "-n", "20",
-            "/home/tomas/workspaces/frinx/odl/autorelease/distribution/distribution-karaf/target/assembly/data/log/karaf.log"
-        ])
-        .output();
-        let output = output.unwrap();
-        assert!(output.status.success(), "tail failed. {:?}", output);
-        let stdout = str::from_utf8(&output.stdout).unwrap();
-        trace!("wait_for_prompt stdout: {}", stdout);
-        if stdout.contains("Prompt resolved") {
-            info!("wait_for_prompt - PromptResolved");
-            return WaitForPromptResult::PromptResolved;
-        }
-        thread::sleep(Duration::from_millis(10));
-    }
-    info!("wait_for_prompt - Timeout");
-    return WaitForPromptResult::Timeout;
-}
-
-#[derive(Debug)]
-enum AfterSleepLogResult {
-    PromptResolved,
-    ExposingMountpointUnder,
-    DeviceSuccessfullyMounted,
-    MountPointAlreadyExistsFailure,
-    Unknown,
-}
-
-fn check_logs_after_sleep() -> AfterSleepLogResult {
+fn get_last_log() -> LogResult {
     let output = Command::new("tail")
     .args(&[
         "-n", "50",
@@ -93,23 +65,38 @@ fn check_logs_after_sleep() -> AfterSleepLogResult {
     let output = output.unwrap();
     assert!(output.status.success(), "tail failed. {:?}", output);
     let stdout = str::from_utf8(&output.stdout).unwrap();
-    trace!("check_logs_after_sleep stdout: {}", stdout);
+    trace!("get_last_log stdout: {}", stdout);
     let result = if stdout.contains("Mount point already exists") {
-        AfterSleepLogResult::MountPointAlreadyExistsFailure
+        panic!("Found the error state");
     } else if stdout.contains("Device successfully mounted") {
-        AfterSleepLogResult::DeviceSuccessfullyMounted
+        LogResult::DeviceSuccessfullyMounted
     } else if stdout.contains("Exposing mountpoint under") {
-        AfterSleepLogResult::ExposingMountpointUnder
+        LogResult::ExposingMountpointUnder
     } else if stdout.contains("Prompt resolved") {
-        AfterSleepLogResult::PromptResolved
+        LogResult::PromptResolved
     } else {
-        AfterSleepLogResult::Unknown
+        LogResult::Unknown
     };
-    debug!("check_logs_after_sleep - {:?}", result);
+    debug!("get_last_log - {:?}", result);
     result
 }
 
-fn unmount() {
+fn wait_for_prompt() -> LogResult {
+    let now = SystemTime::now();
+    let max_duration = Duration::from_secs(15);
+    while now.elapsed().unwrap() < max_duration {
+        if let LogResult::PromptResolved = get_last_log() {
+            info!("wait_for_prompt - PromptResolved");
+            return LogResult::PromptResolved;
+        }
+        thread::sleep(Duration::from_millis(10));
+    }
+    info!("wait_for_prompt - Unknown");
+    return LogResult::Unknown;
+}
+
+
+fn unmount(level: log::Level) {
     let output = Command::new("curl")
         .args(&[
             "-v", "-H", "Content-Type: application/json",
@@ -123,22 +110,16 @@ fn unmount() {
         "Operation mount failed. {:?}",
         output
     );
-    info!("unmount ok");
+    log!(level, "unmount ok");
 }
 
 fn healthcheck() -> bool {
-    if let WaitForPromptResult::PromptResolved = wait_for_prompt() {
+    if let LogResult::PromptResolved = wait_for_prompt() {
         let now = SystemTime::now();
         let max_duration = Duration::from_secs(10);
         while now.elapsed().unwrap() < max_duration {
-            match check_logs_after_sleep() {
-                AfterSleepLogResult::DeviceSuccessfullyMounted => {
-                    return true;
-                }
-                AfterSleepLogResult::MountPointAlreadyExistsFailure => {
-                    panic!("Found the error state");
-                }
-                _ => {}
+            if let LogResult::DeviceSuccessfullyMounted = get_last_log() {
+                return true;
             }
             thread::sleep(Duration::from_millis(10));
         }
@@ -148,28 +129,28 @@ fn healthcheck() -> bool {
 
 fn main() {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
-    let min_duration = Duration::from_millis(70);
-    let max_duration = Duration::from_millis(200); // automatically cut down
+    let min_duration = Duration::from_secs(0);
+    let max_duration = Duration::from_secs(10); // automatically cut down if successfully mounted
 
     loop {
         let mut sleep_duration = min_duration;
         while sleep_duration < max_duration {
-            unmount();
+            unmount(log::Level::Info);
             mount();
-            if let WaitForPromptResult::PromptResolved = wait_for_prompt() {
+            if let LogResult::PromptResolved = wait_for_prompt() {
                 info!("Will sleep {}ms", sleep_duration.as_millis());
                 thread::sleep(sleep_duration);
-                match check_logs_after_sleep() {
-                    AfterSleepLogResult::DeviceSuccessfullyMounted => {
+                match get_last_log() {
+                    LogResult::DeviceSuccessfullyMounted => {
                         break;
                     } // too late, restart
-                    AfterSleepLogResult::MountPointAlreadyExistsFailure => {
-                        panic!("Found the error state");
-                    }
                     _ => {}
                 }
             }
-            unmount();
+            // wait until get prompt is not visible
+            while let LogResult::PromptResolved = get_last_log() {
+                unmount(log::Level::Debug);
+            }
             info!("Executing healthcheck mount");
             // another mount with long wait to make sure we did not enter the race on previous line
             mount();
